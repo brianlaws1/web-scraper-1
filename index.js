fbe9335eb8018e53457792ea8a70ca7bb74aeabf
boots42069420 <brianlaws1@hotmail.com>
2020-11-06 19:53:06 +0000

    added some comments

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+//load axios and cheerio modules
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const getPostTitles = async () => {
 	try {
+    /*'await' keyword replaces the .then promise
+    it is only allowed in a async() block
+    makes sure function waits for data to return
+    before proceeding with the next instruction */
+    //********************* */
+    // {data} gets axios.data
 		const { data } = await axios.get(
 			'https://old.reddit.com/r/programming/'
 		);
+    //look at all the data that is gathered
+    //its html
+    console.log(data);
 		const $ = cheerio.load(data);
 		const postTitles = [];
 
